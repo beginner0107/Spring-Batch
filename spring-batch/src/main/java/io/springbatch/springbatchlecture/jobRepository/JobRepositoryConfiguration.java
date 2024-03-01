354f1d9c2c0f046ea5866ce056680e62401c903e
Beginner <dkstmdwn17@naver.com>
2024-03-02 00:01:03 +0900

    JobRepository 기능, BasicBatchConfig, JobExecutionListener 커스텀 활용

---
+++ spring-batch/src/main/java/io/springbatch/springbatchlecture/jobRepository/JobRepositoryConfiguration.java
@@ -0,0 +1,50 @@
+package io.springbatch.springbatchlecture.jobRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class JobRepositoryConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final JobExecutionListener jobRepositoryListener;
+
+    @Bean
+    public Job BatchJob() {
+        return this.jobBuilderFactory.get("batchJob")
+            .start(step1())
+            .next(step2())
+            .listener(jobRepositoryListener)
+            .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+            .tasklet(
+                ((contribution, chunkContext) -> {
+                System.out.println("step1 was executed");
+                return RepeatStatus.FINISHED;})
+            )
+            .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+            .tasklet(
+                ((contribution, chunkContext) -> {
+                System.out.println("step2 was executed");
+                return RepeatStatus.FINISHED;})
+            ).build();
+    }
+}
